2d23b6d44373d8f8ecc6ecf243e612fbb7880475
maokeduolai <xvlihang@gmail.com>
2024-01-05 21:31:35 +0800

    设置了窗口标题，全局字体改为思源黑体

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    // 设置全局字体
+    QFont font("Source Han Sans CN", 10);
+    QApplication::setFont(font);
+
     Application w;
     w.show();
     return QApplication::exec();
